08bf057350bb2e6d9e9f4ff914a732b66f1b0ed1
Thirds <thirdsgames2018@gmail.com>
2020-05-19 11:50:05 +0100

    Implement render batch

--- covalent/src/graphics/mod.rs
+++ covalent/src/graphics/mod.rs
@@ -1,9 +1,6 @@
 mod backend;
 pub use backend::Backend;
 
-mod primitives;
-pub use primitives::*;
-
 mod render_target;
 pub use render_target::RenderTarget;
 
@@ -14,22 +11,12 @@ pub use pipeline::RenderSettings;
 
 use cgmath::Vector3;
 
-/// Covalent uses batched rendering. Objects are rendered to a batch, which should populate the GPU
-/// with the objects. When dropped, the batch should then tell the GPU to perform the render.
-/// Rendering in batches is much faster than in some kind of "immediate mode" (see OpenGL 2 vs 3).
-///
-/// While this is the intended behaviour of the `RenderContext`, backends are free to in fact use an immediate
-/// mode rendering method, where batched rendering is unsupported.
-/// 
-/// A new `RenderContext` should be created every frame. This does not imply that a new VBO, IBO etc. should be created
-/// every frame by the graphics backend; this is just a convenient API.
-pub trait RenderContext {
-    fn render_tri(&mut self, a: &RenderVertex, b: &RenderVertex, c: &RenderVertex);
-}
-
-/// This is the trait for objects that can be rendered using a RenderContext.
-pub trait Renderable {
-    fn render(&self, rc: &mut impl RenderContext);
+/// This is the trait for objects that can be rendered using a RenderContext. The graphics backend will render these.
+pub enum Renderable {
+    /// A renderable that renders nothing. Used when a Renderable must be supplied but you don't want to render anything.
+    None,
+    /// A primitive triangle containing three vertices.
+    Triangle(RenderVertex, RenderVertex, RenderVertex)
 }
 
 /// Contains all the necessary information to define a single vertex.
--- covalent_gl/src/lib.rs
+++ covalent_gl/src/lib.rs
@@ -3,7 +3,12 @@ use glium::glutin;
 use covalent::cgmath::Vector3;
 use covalent::DisplayHints;
 use covalent::graphics;
-use covalent::graphics::{Pipeline, PipelinePhase, RenderContext, RenderTarget, RenderSettings};
+use covalent::graphics::{Pipeline, PipelinePhase, RenderTarget, RenderSettings, RenderVertex, Renderable};
+
+/// Max vertices to store in a single VBO.
+const MAX_VERTS : usize = 10_000;
+/// Max indices to store in a single IBO.
+const MAX_INDS : usize = 10_000;
 
 /// BackendGL is a rendering backend for Covalent, using OpenGL.
 pub struct BackendGL;
@@ -48,8 +53,8 @@ impl graphics::Backend for BackendGL {
         "#;
         let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-        let vbo = glium::VertexBuffer::dynamic(&display, &vec![Vertex { position: [0.0, 0.0] }; 100]).unwrap();
-        let ibo = glium::index::IndexBuffer::dynamic(&display, glium::index::PrimitiveType::TrianglesList, &vec![0u32; 100]).unwrap();
+        let vbo = glium::VertexBuffer::dynamic(&display, &vec![Vertex { position: [0.0, 0.0] }; MAX_VERTS]).unwrap();
+        let ibo = glium::index::IndexBuffer::dynamic(&display, glium::index::PrimitiveType::TrianglesList, &vec![0u32; MAX_INDS]).unwrap();
         let mut batch = BatchGL {
             vbo: vbo,
             ibo: ibo,
@@ -91,6 +96,13 @@ impl graphics::Backend for BackendGL {
     }
 }
 
+/// Convert a generic RenderVertex into an OpenGL-compatible vertex.
+fn conv(v: &RenderVertex) -> Vertex {
+    Vertex {
+        position: [v.pos.x, v.pos.y]
+    }
+}
+
 impl BackendGL {
     fn execute_phase(&self, _name: &str, phase: &PipelinePhase, batch: &mut BatchGL, frame: &mut glium::Frame) {
         match phase {
@@ -106,22 +118,73 @@ impl BackendGL {
     }
 
     fn render(&self, _settings: &RenderSettings, render_target: &mut impl glium::Surface, batch: &mut BatchGL) {
-        {
-            let mut rc = RenderContextGL {
-                vbo: batch.vbo.map_write(),
-                ibo: batch.ibo.map_write()
-            };
-            
-            rc.render_tri(
-                &graphics::RenderVertex { pos: Vector3 { x: -0.8, y: 1.0, z: 0.0 } },
-                &graphics::RenderVertex { pos: Vector3 { x: 0.9, y: -0.4, z: 0.0 } },
-                &graphics::RenderVertex { pos: Vector3 { x: 0.1, y: -0.8, z: 0.0 } }
-            );
+        let mut scene = Vec::new();
+        for i in (-100..100).map(|x| x as f32) {
+            for j in (-100..100).map(|x| x as f32) {
+                scene.push(Renderable::Triangle(
+                    RenderVertex { pos: Vector3 { x: i * 0.01, y: j * 0.01, z: 0.0 } },
+                    RenderVertex { pos: Vector3 { x: i * 0.01 + 0.008, y: j * 0.01, z: 0.0 } },
+                    RenderVertex { pos: Vector3 { x: i * 0.01, y: j * 0.01 + 0.008, z: 0.0 } }
+                ));
+                scene.push(Renderable::Triangle(
+                    RenderVertex { pos: Vector3 { x: i * 0.01 + 0.008, y: j * 0.01 + 0.008, z: 0.0 } },
+                    RenderVertex { pos: Vector3 { x: i * 0.01 + 0.008, y: j * 0.01, z: 0.0 } },
+                    RenderVertex { pos: Vector3 { x: i * 0.01, y: j * 0.01 + 0.008, z: 0.0 } }
+                ));
+            }
         }
 
-        let params = Default::default();
-        render_target.draw(&batch.vbo, &batch.ibo.slice(0 .. 6).unwrap(), &batch.program, &glium::uniforms::EmptyUniforms, &params).unwrap();
+        let mut it = scene.iter().peekable();
+
+        while let Some(_) = it.peek() {
+            let mut vbo = batch.vbo.map_write();
+            let mut ibo = batch.ibo.map_write();
+            let idx = render_lots(&mut it, &mut vbo, &mut ibo);
+            drop(vbo);
+            drop(ibo);
+
+            if idx > 0 {
+                let params = Default::default();
+                render_target.draw(&batch.vbo, &batch.ibo.slice(0 .. idx).unwrap(), &batch.program, &glium::uniforms::EmptyUniforms, &params).unwrap();
+            }
+        }
     }
+}
+
+/// Render as many things from the given iterator as we can in the current batch, returning the (exclusive) max index we wrote to.
+fn render_lots(
+    it: &mut std::iter::Peekable<std::slice::Iter<'_, Renderable>>,
+    vbo: &mut glium::buffer::WriteMapping<[Vertex]>,
+    ibo: &mut glium::buffer::WriteMapping<[u32]>) -> usize {
+    let mut current_vertex = 0;
+    let mut current_index = 0;
+    loop {
+        match it.peek() {
+            Some(r) => {
+                match r {
+                    Renderable::None => {
+                        it.next();
+                    },
+                    Renderable::Triangle(v0, v1, v2) => {
+                        if current_index + 3 >= MAX_INDS || current_vertex + 3 >= MAX_VERTS {
+                            break  // Do not consume the triangle, leave it to the next call to render_lots.
+                        }
+                        vbo.set(current_vertex + 0, conv(&v0));
+                        vbo.set(current_vertex + 1, conv(&v1));
+                        vbo.set(current_vertex + 2, conv(&v2));
+                        ibo.set(current_index + 0, (current_vertex + 0) as u32);
+                        ibo.set(current_index + 1, (current_vertex + 1) as u32);
+                        ibo.set(current_index + 2, (current_vertex + 2) as u32);
+                        current_vertex += 3;
+                        current_index += 3;
+                        it.next();
+                    }
+                }
+            },
+            None => break
+        }
+    }
+    current_index
 }
 
 #[derive(Copy, Clone)]
@@ -134,30 +197,4 @@ struct BatchGL {
     vbo: glium::VertexBuffer<Vertex>,
     ibo: glium::IndexBuffer<u32>,
     program: glium::Program,
-}
-
-struct RenderContextGL<'a> {
-    vbo: glium::buffer::WriteMapping<'a, [Vertex]>,
-    ibo: glium::buffer::WriteMapping<'a, [u32]>
-}
-
-impl graphics::RenderContext for RenderContextGL<'_> {
-    fn render_tri(&mut self, a: &graphics::RenderVertex, b: &graphics::RenderVertex, c: &graphics::RenderVertex) {
-        let vbo = &mut self.vbo;
-        let ibo = &mut self.ibo;
-
-        vbo.set(0, Vertex {
-            position: [ a.pos.x, a.pos.y ]
-        });
-        vbo.set(1, Vertex {
-            position: [ b.pos.x, b.pos.y ]
-        });
-        vbo.set(2, Vertex {
-            position: [ c.pos.x, c.pos.y ]
-        });
-
-        ibo.set(0, 0u32);
-        ibo.set(1, 1u32);
-        ibo.set(2, 2u32);
-    }
 }
